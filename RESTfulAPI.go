65fe2bb2a4f0b912a4dca247bbfb670d6be4e1a7
Mansi Kalra <54201347+mansikalra23@users.noreply.github.com>
2020-10-29 11:25:13 +0530

    Add files via upload

--- RESTfulAPI.go
+++ RESTfulAPI.go
@@ -16,24 +16,28 @@ type Article struct {
 type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
+	// entering the dummy data
 	articles := Articles{
 		Article{Title: "KloudOne", Desc: "Trainee", Content: "Welcome"},
 	}
 
 	fmt.Println("Endpoint hit: All articles endpoint")
-	json.NewEncoder(w).Encode(articles)
+	json.NewEncoder(w).Encode(articles) // encodes articles array into json string
 }
 
+// homepage function handle all requests to our root URL
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is homepage.")
 }
 
+// handleRequests function match the URL path hit with a defined function
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
+// main function will kick off the API
 func main() {
 	handleRequests()
 }
